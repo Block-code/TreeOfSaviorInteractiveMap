aedcd3b1ab602a66e503282599d5910c6cff4aee
Diego Victor <diegovictorgonzaga@gmail.com>
2017-05-15 18:53:25 -0300

    Load TreeOfSavior plugin

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -66,6 +66,9 @@ Cache::config('default', array('engine' => 'File'));
  * CakePlugin::loadAll(); // Loads all plugins at once
  * CakePlugin::load('DebugKit'); // Loads a single plugin named DebugKit
  */
+CakePlugin::loadAll(array(
+	'TreeOfSavior' => array('routes' => true)
+));
 
 /**
  * To prefer app translation over plugin translation, you can set
