58161590db5b0fe73dfb430dcfab6bcb7b79017a
Diego Victor <diegovictorgonzaga@gmail.com>
2017-05-27 12:29:24 -0300

    Create the drag and drop behavior

--- app/webroot/js/maps.js
+++ app/webroot/js/maps.js
@@ -4,5 +4,27 @@ tosMap.controller('MapsController', [
 		Location.query().$promise.then(function (response) {
 			$scope.locations = response;
 		});
+
+		// Create the drag and move behavior
+		$(document).ready(function(){
+			var wndow, scrolling = false;
+
+			$(window).on('mousedown', function(event) {
+				wndow = {x:event.pageX, y: event.pageY};
+				scrolling = true;
+			});
+
+			$(window).on('mousemove', function(event) {
+				if (scrolling) {
+					window.scrollBy(wndow.x - event.pageX, wndow.y - event.pageY);
+					wndow = {x:event.pageX, y: event.pageY};
+				}
+			});
+
+			$(window).on('mouseup', function () {
+				scrolling = false;
+				wndow = {};
+			});
+		});
 	}
 ]);
