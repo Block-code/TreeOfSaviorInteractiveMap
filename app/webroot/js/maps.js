0690e1c1791ec1c94f44121e4b49eb1b119fb968
Diego Victor <diegovictorgonzaga@gmail.com>
2017-05-28 16:41:23 -0300

    Search and navigate

--- app/webroot/js/maps.js
+++ app/webroot/js/maps.js
@@ -5,6 +5,31 @@ tosMap.controller('MapsController', [
 			$scope.locations = response;
 		});
 
+		// Search for location by name
+		$scope.search = function (q) {
+			var similars = [];
+			angular.forEach($scope.locations, function(location) {
+				if (location.Location.name.match(new RegExp(q, 'i'))) {
+					similars.push({
+						name:location.Location.name,
+						recommended_level: location.Location.recommended_level,
+						x: parseInt(location.Location.position.x, 10),
+						y: parseInt(location.Location.position.y, 10)
+					});
+				}
+			});
+
+			return similars;
+		};
+
+		// Navigate (scroll the window) to the location
+		$scope.navigate = function (location) {
+			window.scrollTo(
+				location.x - Math.floor(window.innerWidth / 2),
+				location.y- Math.floor(window.innerHeight / 2)
+			);
+		};
+
 		// Create the drag and move behavior
 		$(document).ready(function(){
 			var wndow, scrolling = false;
